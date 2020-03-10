2d4ae015ec1889ac5e211633d13a1f9db57bbe71
diogograve90 <diogograve90@gmail.com>
2020-03-10 14:04:04 +0000

    week3

--- mobdev-ionic-intro/src/app/pages/films/films.page.ts
+++ mobdev-ionic-intro/src/app/pages/films/films.page.ts
@@ -1,4 +1,3 @@
-
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
@@ -17,8 +16,11 @@ export class FilmsPage implements OnInit {
 
   ngOnInit() {
     this.films = this.http.get('https://swapi.co/api/films');
-    this.films.subscribe(data => {
-      console.log('my data: ', data);
-    });
+  }
+
+  openDetails(film) {
+    let split = film.url.split('/');
+    let filmId = split[split.length-2];
+    this.router.navigateByUrl(`/tabs/films/${filmId}`);
   }
 }
